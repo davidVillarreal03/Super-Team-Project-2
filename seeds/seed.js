ded4cee9493c3f929f84e6fdf9f55935add0d4fa
rburgos <rachel@pixelstreetmedia.com>
2024-08-16 18:46:22 -0500

    added models Favorites, Genre, Movie.  And added seed files in .js format combined genre tables into one and implemented genre table

---
+++ seeds/seed.js
@@ -0,0 +1,20 @@
+// seed.js
+const sequelize = require('./config/connection');
+const seedGenres = require('./seeds/genreData');
+const seedMovies = require('./seeds/movieData');
+const seedUsers = require('./seeds/userData');
+const seedFavorites = require('./seeds/favoriteData');
+
+const seedAll = async () => {
+    // while in development set to "true" empties tables and repopulates with seed data each time
+  await sequelize.sync({ force: true });
+
+  await seedGenres();
+  await seedMovies();
+  await seedUsers();n
+  await seedFavorites();
+
+  process.exit(0);
+};
+
+seedAll();
