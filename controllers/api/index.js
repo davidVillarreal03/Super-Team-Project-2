4fe48a59d4e1e8dc6baef7edb25a149e2a1aa75c
rburgos <rachel@pixelstreetmedia.com>
2024-08-19 18:43:02 -0500

    modified controllers/api/index.js file

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 
-const userRoutes = require('./userRoutes');
 const movieRoutes = require('./movieRoutes');
+const genreRoutes = require('./genreRoutes');
+const userRoutes = require('./userRoutes');
 
 router.use('/users', userRoutes);
 router.use('/movies', movieRoutes);
