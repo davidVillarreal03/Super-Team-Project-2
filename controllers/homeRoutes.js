3b3708a7a3f5a7dabc5070fa1e3360358e2d1414
LeonathaTurtle <mancillh@gmail.com>
2024-08-22 09:22:54 -0500

    changed Project.js in models folder to Movie.js. In homeRoutes.js, declared router, deconstructed Movie and User, declared withAuth to ensure user is logged in, set up get route for specific movie, included res.render movie, exported router

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,27 @@
+const router = require('express').Router();
+const { Movie, User } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/movie/:id', async (req, res) => {
+  try {
+    const movieData = await Movie.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['title'],
+        },
+      ],
+    });
+
+    const movie = movieData.get({ plain: true });
+
+    res.render('movie', {
+      ...movie,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
