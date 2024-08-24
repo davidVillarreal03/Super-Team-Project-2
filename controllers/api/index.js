e97d471a76cdfe59473a688068a7c5cb984b637b
LeonathaTurtle <mancillh@gmail.com>
2024-08-24 13:23:17 -0500

    favoriteRoutes instead of movieRoutes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 
-const movieRoutes = require('./movieRoutes');
+const favoriteRoutes = require('./favoriteRoutes');
 const userRoutes = require('./userRoutes');
 
 router.use('/users', userRoutes);
-router.use('/movies', movieRoutes);
+router.use('/favorites', favoriteRoutes);
 
 module.exports = router;
