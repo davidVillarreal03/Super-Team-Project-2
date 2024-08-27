b05c713573733f843becb18dd963ba049244959b
LeonathaTurtle <mancillh@gmail.com>
2024-08-27 14:37:15 -0500

    fixed favorite route

--- public/js/login.js
+++ public/js/login.js
@@ -13,7 +13,7 @@ const signupFormHandler = async (event) => {
       });
   
       if (response.ok) {
-        document.location.replace('/favorite');
+        document.location.replace('/api/favorite');
       } else {
         alert(response.statusText);
       }
