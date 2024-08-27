f8970f18a05c5113baad1198a848b521bf8ee434
LeonathaTurtle <mancillh@gmail.com>
2024-08-27 14:50:10 -0500

    added button and logic to return user home when pressed to favorites page

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,11 +1,10 @@
 const logout = async (event) => {
   event.preventDefault();
-  console.log('logout here ++++++++++++++++++++++++++++++++++++++++++');
+  
   const response = await fetch('/api/users/logout', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
   });
-  console.log({ response });
   if (response.ok) {
     document.location.replace('/login');
   } else {
